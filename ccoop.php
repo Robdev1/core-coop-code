769dc54a934c3223d13cd6dfeb22ae497a3ca993
Robert Thomason <robert.thomason@gmail.com>
2015-08-23 22:06:09 -0400

    Adds Scenario 3 of 4321 4-Member Breakdown. Also standardizes formatting procedures for currency and percentages.

--- ccoop.php
+++ ccoop.php
@@ -1,20 +1,18 @@
 <HEAD>
 <STYLE>
-
 body {
     background-color: linen;
     font-family: Helvetica, "Nimbus Sans L", "Liberation Sans", Arial, sans-serif;
      width:960px;
-}
+     }
 
 h1 {
     color: maroon;
-} 
-</style>
+   } 
+</STYLE>
 
  <meta name="description" content="This file demonstrates the core economics of a cooperative. It uses variables, such as capitalization, number of members, income and redistribution of income.">
-
-</head>
+</HEAD>
 
 <BODY>
 <h1>Core Cooperative Code</h1>
@@ -22,78 +20,52 @@ h1 {
 
 <!- Enters key variables ->
 <?php
-if (isset($_GET['membersnum2']))
+if (isset($_GET['membersnum']))
    {
-     $membersnum = $_GET['membersnum2'];
+     $membersnum = $_GET['membersnum'];
    } else {
      $membersnum = 10;
+   }   if (isset($_GET['capital1']))
+   {
+     $capital1 = $_GET['capital1'];
+   } else {
+     $capital1 = 10000;
    }
-?>
+$capital1output = number_format($capital1, 2);?>
 
 <FORM METHOD="GET" ACTION="ccoop.php">
 Initial Capitalization:<br>
 <TEXTAREA NAME="capital1" ROWS=1 COLS=15
-WRAP=soft>100000</textarea>
+WRAP=soft><?php echo $capital1 ?></textarea>
 <br>
 Number of Members:<br>
-<TEXTAREA NAME="membersnum2" ROWS=1 COLS=4
+<TEXTAREA NAME="membersnum" ROWS=1 COLS=4
 WRAP=soft><?php echo $membersnum ?></textarea>
 
 <?php
 $capital1 = $_GET['capital1'];
 $membersfraction = 1/$membersnum;
-
-function percent($membersfraction){
-    return $membersfraction * 100 . '%';
+# This function takes a decimal number, calculates its as a percent and returns it to two decimal places, appending the percentage sign on it for outputting in a line of text.
+function percent($decival){
+    return number_format($decival * 100, 2) . '%';
 }
 
-$memberinvestment = $capital1/$membersnum;
+$memberinvestment = number_format(($capital1/$membersnum) , 2);
 
 ?>
-
-<p>
-Percentage Ownership of Members if All Have Equal Shares:<br>
- <?php echo percent($membersfraction) ?>
-
-<P>
-<!-- The following takes entered values of income and capitalgain and sums them into return. Then the return per share of ownership is calculated and displayed. -->
-
-Income:<br>
-<TEXTAREA NAME="income" ROWS=1 COLS=15
-WRAP=soft>25000</textarea>
-<br>
-Capital Gain:<br>
-<TEXTAREA NAME="capitalgain" ROWS=1 COLS=15
-WRAP=soft>25000</textarea>
-<br>
-
-<?php
-$income = $_GET['income'];
-$capitalgain = $_GET['capitalgain'];
-$return = $income + $capitalgain;
-?>
-
-<hr>
-Calculate the following metrics based on figures entered above.
-<p>
-Amount invested by each member: $<?php echo $memberinvestment ?>
-<p>
-
-Return: $<?php echo $return ?><p>
-<!-- <TEXTAREA NAME="return" ROWS=1 COLS=15
-WRAP=soft><?php echo $return ?></textarea>
-<br> -->
-
-<?php
-$returnmembershare = $return * $membersfraction;
-?>
-
-Share of Return per Member: $<?php echo $returnmembershare ?><p>
-<!-- <TEXTAREA NAME="returnmembershare" ROWS=1 COLS=15
-WRAP=soft><?php echo $returnmembershare ?></textarea>
-<br> --><hr><!-- The following section gives an alternative percentage of ownership -->If one member holds 51% and the remaining shares are divided equally among the others the breakdown is:<p><?php$amtfor51pcter = $capital1 * .51;$amtfor51pcter = number_format($amtfor51pcter, 2);$numremainmembers = $membersnum - 1;$pctremainmembers = .49/$numremainmembers;$pctremainmembers = number_format(($pctremainmembers * 100), 2);$amtremainmemberstotal = $capital1 * .49;$amtperremainmembers = $amtremainmemberstotal / $numremainmembers;$amtperremainmembers = number_format($amtperremainmembers, 2);echo (" * Member with 51% contributes  $$amtfor51pcter <br>");echo (" * The other  $numremainmembers  contribute  $$amtperremainmembers each for $pctremainmembers%  portion each.<p>");?>
-<input type=submit value=‘submit’>
+<p><input type=submit value=‘submit’>
 <input type=reset  value=‘reset’>
 <BR>
 </FORM>
+<hr><!-- SCENARIO 1 EGALITARIAN --><h3>Scenario 1: Egalitarian</h3>
+Percentage Ownership of Members if All Have Equal Shares: 
+ <?php echo percent($membersfraction) ?>
+
+<br>
+Amount contributed by each member: $<?php echo $memberinvestment ?>
+<br>
+
+<hr><!-- SCENARIO 2 MAJORDOMO  - This looks at the effect of one majority stakeholder --><h3>Scenario 2: Majordomo with 51% Majority</h3>
+If one member holds 51%, and the remaining shares are divided equally among the others:<p><?php$amtfor51pcter = $capital1 * .51;$amtfor51pcter = number_format($amtfor51pcter, 2, ".", ",");$numremainmembers = $membersnum - 1;$remainderdecival = .49/$numremainmembers;$amtremainmemberstotal = $capital1 * .49;$amtperremainmembers = number_format(($amtremainmemberstotal / $numremainmembers), 2);echo (" * The majority stakeholder (majordomo) member with 51% ownership contributes  $$amtfor51pcter <br>");echo (" * The other  $numremainmembers  contribute  $$amtperremainmembers each for "); echo percent($remainderdecival); echo (" portion each.<p>");?><hr><!-- SCENARIO 3 4321BFEAKDOWN--><h3>Scenario 3: 40% - 30% - 20% - 10% Ownership (4321 4-Member Breakdown)</h3><?php$membersnum4321 = 4;$member4pct = number_format((.4 * 100));$member3pct = number_format((.3 * 100));$member2pct = number_format((.2 * 100));$member1pct = number_format((.1 * 100));?>If the ownership breaks down so that four members have <?php echo (" $member4pct%, $member3pct%, $member2pct% and $member1pct%"); ?> respectively, then,<br>the members must contribute:<br><?php$member4contrib = number_format((.4 * $capital1), 2);$member3contrib = number_format((.3 * $capital1), 2);$member2contrib = number_format((.2 * $capital1), 2);$member1contrib = number_format((.1 * $capital1), 2);echo (" $ $member4contrib <br> $ $member3contrib <br> $ $member2contrib <br> $ $member1contrib <br> in order for the venture to raise $$capital1output.<p>");?>
+
 </BODY>
